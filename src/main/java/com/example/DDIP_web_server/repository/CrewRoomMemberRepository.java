bd9db03cf4dc53e81690ed087e0dddcdae625dda
hae <godma531@naver.com>
2024-11-18 22:59:23 +0900

    DB 변경으로 인한 오류 수정을 위한 작업파일

--- src/main/java/com/example/DDIP_web_server/repository/CrewRoomMemberRepository.java
+++ src/main/java/com/example/DDIP_web_server/repository/CrewRoomMemberRepository.java
@@ -7,6 +7,4 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface CrewRoomMemberRepository extends JpaRepository<CrewRoomMember, Integer> {
 
-    // memberId를 기준으로 CrewRoomMember 엔터티를 찾는 메서드
-    CrewRoomMember findByMemberId(String member_id);
 }
--- src/main/java/com/example/DDIP_web_server/service/CrewRoomScheduleService.java
+++ src/main/java/com/example/DDIP_web_server/service/CrewRoomScheduleService.java
@@ -2,6 +2,7 @@ package com.example.DDIP_web_server.service;
 import com.example.DDIP_web_server.entity.CrewRoomMember;
 import com.example.DDIP_web_server.entity.CrewRoomSchedule;
 import com.example.DDIP_web_server.repository.CrewRoomMemberRepository;
+import com.example.DDIP_web_server.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.example.DDIP_web_server.repository.CrewRoomScheduleRepository;
@@ -15,18 +16,19 @@ public class CrewRoomScheduleService {
 
     @Autowired
     private CrewRoomScheduleRepository crewScheduleRepository;
-
+    @Autowired
+    private MemberRepository memberRepository;
     @Autowired
     private CrewRoomMemberRepository crewRoomMemberRepository;
 
-    public CrewRoomMember getCrewRoomMemberByMemberId(int memberId) {
-        CrewRoomMember crewRoomMember = crewRoomMemberRepository.findByMemberId(String.valueOf(memberId));
+/*    public int getPayByMemberId(String memberId) {
+        CrewRoomMember crewRoomMember = crewRoomMemberRepository.findByMemberId(memberId);
         if (crewRoomMember != null) {
-            return crewRoomMember;
+            return crewRoomMember.getPay();
         } else {
-            throw new RuntimeException("CrewRoomMember not found for memberId: " + memberId);
+            throw new RuntimeException("Pay information not found for memberId: " + memberId);
         }
-    }
+    }*/
 
     @Transactional
     public void saveSchedule(CrewRoomSchedule schedule) {
