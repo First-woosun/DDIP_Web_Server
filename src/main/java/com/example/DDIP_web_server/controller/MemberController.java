b016e934dd27074118a776a2448ea0ca781dca17
hae <godma531@naver.com>
2024-11-25 19:59:02 +0900

    병합 금지

--- src/main/java/com/example/DDIP_web_server/controller/MemberController.java
+++ src/main/java/com/example/DDIP_web_server/controller/MemberController.java
@@ -13,7 +13,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/Member")
 public class MemberController {
 
     @Autowired
@@ -22,12 +22,12 @@ public class MemberController {
     @Autowired
     private MemberRepository memberRepository;
 
-    @PostMapping("/Member/signup")
+    @PostMapping("/signup")
     public Member signup(@RequestBody Member user) {
         return memberService.signup(user);
     }
 
-    @PostMapping("/Member/login")
+    @PostMapping("/login")
     public Member login(@RequestParam String id, @RequestParam String password) {
         Member user = memberService.login(id, password);
         if (user != null) {
